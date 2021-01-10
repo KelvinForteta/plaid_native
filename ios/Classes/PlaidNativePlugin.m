4b3040c15ed92aac561284d69be2520e3762accb
Kelvin Forteta <kelvin.forteta@gmail.com>
2021-01-10 14:31:04 +0100

    initial commit

---
+++ ios/Classes/PlaidNativePlugin.m
@@ -0,0 +1,15 @@
+#import "PlaidNativePlugin.h"
+#if __has_include(<plaid_native/plaid_native-Swift.h>)
+#import <plaid_native/plaid_native-Swift.h>
+#else
+// Support project import fallback if the generated compatibility header
+// is not copied when this plugin is created as a library.
+// https://forums.swift.org/t/swift-static-libraries-dont-copy-generated-objective-c-header/19816
+#import "plaid_native-Swift.h"
+#endif
+
+@implementation PlaidNativePlugin
++ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
+  [SwiftPlaidNativePlugin registerWithRegistrar:registrar];
+}
+@end
---
+++ ios/Classes/SwiftPlaidNativePlugin.swift
@@ -0,0 +1,14 @@
+import Flutter
+import UIKit
+
+public class SwiftPlaidNativePlugin: NSObject, FlutterPlugin {
+  public static func register(with registrar: FlutterPluginRegistrar) {
+    let channel = FlutterMethodChannel(name: "plaid_native", binaryMessenger: registrar.messenger())
+    let instance = SwiftPlaidNativePlugin()
+    registrar.addMethodCallDelegate(instance, channel: channel)
+  }
+
+  public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+    result("iOS " + UIDevice.current.systemVersion)
+  }
+}
